2821acc2a3464d51f48464a8bfe65f149195382b
양서린 <bisor0627@gmail.com>
2025-02-11 11:51:26 +0900

    🔒 환경 변수(.env) 도입 및 Slack Webhook URL 보호
    **Why:**
    - Slack Webhook URL이 코드에 노출되어 있어 보안 취약점이 발생할 가능성이 있음.
    - Webhook URL을 `.env` 파일로 관리하여 보안성을 강화하고 관리 용이성을 높이고자 함.
    
    **What:**
    - `.env` 파일을 사용하여 Slack Webhook URL을 환경 변수로 저장하도록 변경.
    - `flutter_dotenv` 패키지를 추가하여 환경 변수를 로드하도록 수정.
    - `SlackLogService`에서 `.env` 파일에서 Webhook URL을 가져오도록 변경.
    - `.gitignore`에 `*.env` 추가하여 환경 변수가 Git에 커밋되지 않도록 설정.
    
    **How:**
    - `pubspec.yaml`에 `flutter_dotenv: ^5.2.1` 추가 및 `pubspec.lock` 업데이트.
    - `lib/main.dart`에서 `dotenv.load(fileName: ".env")` 호출하여 환경 변수 로드.
    - `SlackLogService`에서 `dotenv.env['SLACK_WEBHOOK_URL']`을 사용하여 Webhook URL 설정.
    - `.gitignore`에 `.env` 추가하여 민감한 정보가 저장소에 포함되지 않도록 조치.
    
    **Tags:**
    #security #dotenv #slack #logging #refactoring

--- lib/data/datasources/remote/slack_log_service.dart
+++ lib/data/datasources/remote/slack_log_service.dart
@@ -1,39 +1,44 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class SlackLogService {
   static final SlackLogService _instance = SlackLogService._internal();
   factory SlackLogService() => _instance;
 
+  final slackWebhookUrl = dotenv.env['SLACK_WEBHOOK_URL'];
   SlackLogService._internal() {
     init();
   }
 
-  final String slackWebhookUrl =
-      "https://hooks.slack.com/services/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"; // Webhook URL
   void init() {
     sendLogToSlack("🚀 Flutter App Started!");
   }
 
   Future<void> sendLogToSlack(String message) async {
-    final payload = jsonEncode({"text": message});
+    if (slackWebhookUrl == null) {
+      log("❌ Slack Webhook URL이 없습니다.");
+      return;
+    } else {
+      final payload = jsonEncode({"text": message});
 
-    try {
-      final response = await http.post(
-        Uri.parse(slackWebhookUrl),
-        headers: {"Content-Type": "application/json"},
-        body: payload,
-      );
+      try {
+        final response = await http.post(
+          Uri.parse(slackWebhookUrl!),
+          headers: {"Content-Type": "application/json"},
+          body: payload,
+        );
 
-      if (response.statusCode != 200) {
-        log("❌ Slack Webhook 오류: ${response.body}");
+        if (response.statusCode != 200) {
+          log("❌ Slack Webhook 오류: ${response.body}");
+          log("curl -X POST -H \"Content-Type: application/json\" -d '$payload' $slackWebhookUrl");
+        }
+      } catch (e) {
+        log("❌ Slack Webhook 오류: $e");
         log("curl -X POST -H \"Content-Type: application/json\" -d '$payload' $slackWebhookUrl");
       }
-    } catch (e) {
-      log("❌ Slack Webhook 오류: $e");
-      log("curl -X POST -H \"Content-Type: application/json\" -d '$payload' $slackWebhookUrl");
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,19 @@ import 'dart:io';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_snaptag_kiosk/lib.dart';
 import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
   if (kDebugMode) {
     F.appFlavor = Flavor.dev;
   } else {
     F.appFlavor = Flavor.prod;
   }
+  await dotenv.load(fileName: "assets/.env");
   final slackCall = SlackLogService();
 
   // Zone으로 감싸서 모든 비동기 에러도 캐치
